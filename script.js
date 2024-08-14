df55728720717450f5936ec8b97727dd5e4746a9
Agus1441 <agustin.schlechter@correo.ucu.ed>
2024-08-14 09:31:33 -0300

    commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function repeatString(texto, repeticiones){
+    for(let repeticion = 0; repeticion < repeticiones; repeticion++){
+        console.log(texto);
+    }
+}
+
+repeatString("hola mundo", 5);
